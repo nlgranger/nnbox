f4abb0e154db13305b70f55774ddaced2abc36b5
Nicolas Granger <nicolas.granger@telecom-sudparis.eu>
2015-07-22 10:51:26 +0200

    changed L2 related fonctions to true L2 norm (not squared)

--- L2Compare.m
+++ L2Compare.m
@@ -21,9 +21,10 @@ classdef L2Compare < handle & AbstractNet
             nSamples = size(X{1}, numel(self.inSize) + 1);
             X{1} = reshape(X{1}, [], nSamples);
             X{2} = reshape(X{2}, [], nSamples);
-            Y    = sum((X{1} - X{2}) .^2, 1);
+            Y    = sqrt(sum((X{1} - X{2}) .^2, 1));
             if nargout > 1
-                A = X{1} - X{2};
+                A.D = X{1} - X{2};
+                A.Y = Y;
             end
         end
         
@@ -31,11 +32,10 @@ classdef L2Compare < handle & AbstractNet
         end
         
         function [G, inErr] = backprop(self, A, outErr)
-            G = [];
-            inErr = { bsxfun(@times, 2 * A, outErr), ...
-                - bsxfun(@times, 2 * A, outErr)};
-            inErr{1} = reshape(inErr{1}, [self.inSize numel(outErr)]);
-            inErr{2} = reshape(inErr{2}, [self.inSize numel(outErr)]);
+            G     = [];
+            D     = bsxfun(@times, A.D, outErr ./ A.Y);
+            D     = reshape(D, [self.inSize numel(outErr)]);
+            inErr = {D, -D};
         end
         
         function [] = gradientupdate(~, ~)
--- L2Cost.m
+++ L2Cost.m
@@ -3,11 +3,12 @@ assert(isnumeric(Y), 'Only numeric O and Y are supported');
 
 nSamples = size(O, ndims(O));
 if isempty(varargin)
-    C = mean(sum(reshape((O - Y) .^ 2, [], nSamples), 1));
+    C = mean(sqrt(sum(reshape((O - Y) .^ 2, [], nSamples), 1)));
 elseif strcmp(varargin{1}, 'each')
-    C = sum(reshape((O - Y) .^ 2, [], nSamples), 1);
+    C = sqrt(sum(reshape((O - Y) .^ 2, [], nSamples), 1));
 elseif strcmp(varargin{1}, 'gradient');
-    C = (O - Y) / nSamples;
+    eachC = sqrt(sum(reshape((O - Y) .^ 2, [], nSamples), 1));
+    C    = bsxfun(@rdivide, O - Y, eachC * nSamples);
 else
     error('Unrecognized optional argument');
 end
