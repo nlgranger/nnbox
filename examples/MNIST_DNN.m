c39982dc41a59d2e4ea616fb8e552a3ba557216a
Nicolas Granger <nicolas.granger@telecom-sudparis.eu>
2015-10-12 15:08:08 +0200

    updated example to give final scores and results

--- examples/MNIST_DNN.m
+++ examples/MNIST_DNN.m
@@ -3,17 +3,19 @@
 % fast learning algorithm for deep belief nets. Neural computation, 18(7),
 % 1527-1554.
 
-% Load Database
+%% Load Database
 [trainX, trainY, testX, testY] = getMNIST();
 trainX = double(reshape(trainX, 28*28, 60000)) / 255;
 trainY = ((0:9)' * ones(1, 60000)) == (ones(10, 1) * double(trainY'));
 testX  = double(reshape(testX, 28*28, 10000)) / 255;
 testY  = ((0:9)' * ones(1, 10000)) == (ones(10, 1) * double(testY'));
 
-% Setup network
+%% Setup network
 
+% empty multilayer network squeleton
 net  = MultiLayerNet();
 
+% setup first layer
 pretrainOpts = struct( ...
     'nEpochs', 50, ...
     'momentum', 0.7, ...
@@ -25,18 +27,23 @@ trainOpts = struct( ...
     'lRate', 5e-4, ...
     'batchSz', 200);
 rbm1 = RBM(28*28, 500, pretrainOpts, trainOpts);
+
+% add first layer
 net.add(rbm1);
 
+% setup second layer
 pretrainOpts.nEpochs = 15;
 trainOpts = struct( ...
     'lRate', 5e-4, ...
     'batchSz', 200);
 rbm2 = RBM(500, 500, pretrainOpts, trainOpts);
+
+% add second layer
 net.add(rbm2);
 
 % Pretrain bottom layers
 fprintf('Pretraining first two layer\n');
-net.pretrain(trainX);
+net.pretrain(trainX); % LultilayerNet is configured to pretrain layerwise
 
 % Finish network
 rbm3 = RBM(500, 2000, pretrainOpts, trainOpts);
@@ -45,7 +52,7 @@ net.add(rbm3);
 per  = Perceptron(2000, 10, trainOpts);
 net.add(per);
 
-% Train
+%% Train
 fprintf('Fine-tuning\n');
 
 trainOpts = struct(...
@@ -53,3 +60,31 @@ trainOpts = struct(...
     'batchSz', 500, ...
     'displayEvery', 3);
 train(net, CrossEntropyCost(), trainX, trainY, trainOpts);
+
+%% Results
+disp('Confusion matrix:')
+[~, tmp] = max(net.compute(testX));
+tmp      = tmp - 1; % first class is 0
+Y        = bsxfun(@eq, (0:9)' * ones(1, 10000), tmp);
+confusion = double(testY) * double(Y');
+disp(confusion);
+
+disp('Classification error (testing):');
+disp(mean(sum(Y ~= testY) > 0));
+
+disp('Classification error (training');
+[~, tmp] = max(net.compute(trainX));
+tmp      = tmp - 1; % first class is 0
+Y        = bsxfun(@eq, (0:9)' * ones(1, 60000), tmp);
+disp(mean(sum(Y ~= trainY) > 0));
+
+disp('Showing first layer weights as filter (20 largest L2 norm)');
+weights = net.nets{1}.W;
+[~, order] = sort(sum(weights .^2), 'descend');
+colormap gray
+for i = 1:20
+    subplot(5, 4, i);
+    imagesc(reshape(weights(:, order(i)), 28, 28));
+    axis image
+    axis off
+end
